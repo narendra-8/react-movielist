6a97248dd853b9470e56522dbd8765888b19ba3c
narendra-8 <86469490+narendra-8@users.noreply.github.com>
2022-02-24 19:24:42 +0530

    Update Initial.js

--- src/Initial.js
+++ src/Initial.js
@@ -5,7 +5,8 @@ export const Initial =[
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT69mnaz2z3AeZitFmqCWJFUeujGjGPJzKfkw&usqp=CAU",
     summary:
       "Marvel's Iron Man 3 - pits brash-but-brilliant industrialist Tony Stark/Iron Man against an enemy whose reach knows no bounds. When Stark finds his personal world destroyed at his enemy hands, he embarks on a harrowing quest to find those responsible. This journey, at every turn, will test his mettle.",
-    Ratings: 4
+    Ratings: 4,
+    trailer:"https://www.youtube.com/embed/Ke1Y3P9D0Bc"
   },
   {
     Mname: "Spiderman",
@@ -13,7 +14,8 @@ export const Initial =[
       "https://www.denofgeek.com/wp-content/uploads/2019/07/spider-man-far-from-home-ending-explained.jpg?fit=1920%2C1080",
     summary:
       "Spider-Man is a Marvel commic story, When a human bitten by a genetically modified spider, a nerdy, shy, and awkward high school student gains spider-like abilities that he eventually must use to fight evil as a superhero after tragedy befalls his family.",
-    Ratings: 4.2
+    Ratings: 4.2,
+    trailer:"https://www.youtube.com/embed/rk-dF1lIbIg"
   },
   {
     Mname: "Arjun Reddy",
@@ -21,7 +23,8 @@ export const Initial =[
       "https://th.bing.com/th/id/OIP.qAMaF4LPOpxxC48g9pAyQQHaHa?w=179&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7",
     summary:
       "Arjun Reddy, a medico with anger management issues, gets into a brawl with another student and is asked to either apologize or leave college. Arjun prepares to leave but stays back after seeing a first year student, Preethi Shetty. Arjun, along with his friend Shiva, goes to first year class room and announces that he is in love with Preethi and she is exclusive to him.",
-    Ratings: 4.4
+    Ratings: 4.4,
+    trailer:"https://www.youtube.com/embed/aozErj9NqeE"
   },
 
 {
@@ -30,7 +33,8 @@ export const Initial =[
       "https://www.bing.com/th?id=AMMS_3210edbdc9a03cfab36e034bdc70c9f8&w=123&h=183&c=8&rs=1&o=5&dpr=1.25&pid=3.1&rm=2",
     summary:
       "Dark is a German science fiction thriller streaming television series co-created by Baran bo Odar and Jantje Friese.[5][6][7] It ran for three seasons from 2017 to 2020. In the aftermath of a child's disappearance, Dark follows characters from the fictional German town of Winden as they pursue the truth.",
-    Ratings: 4.5
+    Ratings: 4.5,
+    trailer:"https://www.youtube.com/embed/rrwycJ08PSA"
   },
 
 {
@@ -39,7 +43,8 @@ export const Initial =[
       "https://th.bing.com/th/id/OIP.id0OxKoly7a6yTeRGrgTPgHaEe?w=266&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7",
     summary:
       "An unusual group of robbers attempt to carry out the most perfect robbery in Spanish history - stealing 2.4 billion euros from the Royal Mint of Spain.",
-    Ratings: 4.8
+    Ratings: 4.8,
+    trailer:"https://www.youtube.com/embed/_InqQJRqGW4"
   },
 
 ];
